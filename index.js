e90667a2d0178f711dcfaa175368ef28dae3a5aa
githubscientist <sathish.datascientist@gmail.com>
2024-05-17 20:26:15 +0530

    setup root route using express js

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
-const notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        date: "2019-05-30T17:30:31.098Z",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2019-05-30T18:39:34.091Z",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2019-05-30T19:20:14.298Z",
-        important: true
-    }
-]
+// require the express module
+const express = require('express');
 
-// import http module
-const http = require('http');
+// create a new express application
+const app = express();
 
-// create server object
-// const server = http.createServer((request, response) => {
-//     response.writeHead(200, { 'Content-Type': 'text/plain' });
-//     response.end('Hello World');
-// });
-
-const server = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(notes));
+// define the first route '/'
+app.get('/', (req, res) => {
+    res.send('Hello World!!');
 });
 
-// start the server to listen to requests on port 3001
-server.listen(3001, () => {
-    console.log('Server is running on http://127.0.0.1:3001');
+// start the express application on port 3001
+app.listen(3001, () => {
+    console.log(`Server is running on http://localhost:3001`);
 });
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 - Create a web server using nodejs and http module
+- express module:
+  - used to create a web server
+  - used to create a REST API
+  - easy way compared to http module
