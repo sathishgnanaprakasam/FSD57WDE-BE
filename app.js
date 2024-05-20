a0e603396d4e2a1d6c063ba84bb3bb84d1e7a7a7
githubscientist <sathish.datascientist@gmail.com>
2024-05-20 19:57:16 +0530

    added logger --morgan module

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 // middleware to parse the request body
 app.use(express.json());
+
+// middleware to log the request
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('get request to the homepage');
 });
 
 app.post('/', (req, res) => {
-  console.log(req.body);
   res.send('post request to the homepage');
 });
 
