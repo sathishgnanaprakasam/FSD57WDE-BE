eb12bc0200f63d3e465a1f740707b18625f6869c
githubscientist <sathish.datascientist@gmail.com>
2024-05-20 19:52:05 +0530

    parse the request body

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
+// middleware to parse the request body
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('get request to the homepage');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('post request to the homepage');
 });
 
 // export the app module
