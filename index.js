4437c21a2070e1026140de5490d21778944b5437
githubscientist <sathish.datascientist@gmail.com>
2024-05-20 19:46:14 +0530

    connecting to mongodb

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 // import the app module
 const app = require('./app');
 
-// start the server
-app.listen(3001, () => {
-  console.log('Server is running on http://localhost:3001');
-});
+// import the mongoose module
+const mongoose = require('mongoose');
+
+// log the message connecting to the MongoDB
+console.log('Connecting to MongoDB...');
+
+mongoose.connect(`mongodb+srv://guvi:Guvi2023@atlascluster.nzeb00e.mongodb.net/`)
+  .then(
+    () => {
+      console.log('Connected to MongoDB');
+
+      // start the server
+      app.listen(3001, () => {
+        console.log('Server is running on http://localhost:3001');
+      });
+    }
+  )
+  .catch((error) => {
+    console.error('Error connecting to MongoDB: ', error.message);
+  });
